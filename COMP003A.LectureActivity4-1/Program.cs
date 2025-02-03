c6ac580dedfec0a82e7699e3f77fe3f784b9e28d
briannaordaz <briannaordaz5@gmail.com>
2025-02-02 21:56:32 -0800

    declared an integer array to store grades for 5 students

---
+++ COMP003A.LectureActivity4-1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003A.LectureActivity4_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Declare an array to store 5 grades
+        int[] grade = new int[5];
+        Console.WriteLine("Hello, World!");
+    }
+}
